2ae93a2c38aa30282a01d7a51e45f1c7e79d83b9
Ensar Emir Erol <ensaremir.erol99@gmail.com>
2023-11-23 15:40:08 +0100

    changes

--- changes/7/apply.py
+++ changes/7/apply.py
@@ -70,23 +70,26 @@ def apply(graph: Graph) -> Graph:
     # Execute the DELETE query
     graph.update(delete_query)
 
-    # hasCode
-    # TODO: this is not working
-    # update_query = """
-    #     PREFIX sphn: <https://biomedit.ch/rdf/sphn-ontology/sphn#>
-    #     PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    #     INSERT DATA {
-    #         sphn:Measurement rdfs:subClassOf
-    #         [ a owl:Class ;
-    #             owl:intersectionOf ( [ owl:intersectionOf ( [ a owl:Restriction ;
-    #                         owl:minCardinality "0"^^xsd:nonNegativeInteger ;
-    #                         owl:onProperty sphn:hasCode ] [ a owl:Restriction ;
-    #                         owl:maxCardinality "1"^^xsd:nonNegativeInteger ;
-    #                         owl:onProperty sphn:hasCode ] ) ]
-    #     }
-    # """
+    update_query = """
+        PREFIX sphn: <https://biomedit.ch/rdf/sphn-ontology/sphn#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#> 
+        INSERT DATA {         
+            sphn:Measurement rdfs:subClassOf 
+            [ a owl:Class ;
+                owl:intersectionOf ( 
+                    [ a owl:Restriction ;
+                    owl:minCardinality "0"^^xsd:nonNegativeInteger ;
+                    owl:onProperty sphn:hasCode ] 
+                    [ a owl:Restriction ;
+                        owl:maxCardinality "1"^^xsd:nonNegativeInteger ;
+                    owl:onProperty sphn:hasCode ] ) 
+            ] 
+            
+        
+        }
+    """
 
-    # # Execute the DELETE query
-    # graph.update(delete_query)
+    # Execute the DELETE query
+    graph.update(update_query)
 
     return graph
